57c79ee2aef7934d42059295189c99ddf1b6e736
Mark Weerheim <theDutchFlamingo@gmail.com>
2020-04-27 12:13:20 +0200

    Made two plots in one graph
    It looks better, but still completely 100% wrong

--- Python/Network.py
+++ Python/Network.py
@@ -11,17 +11,12 @@ It = 1000
 ratios = []
 ratios2 = []
 
-for i, n in enumerate(N):
+for j, p in enumerate(P):
     row = []
     row2 = []
 
-    for j, p in enumerate(P):
+    for i, n in enumerate(N):
         G = [nx.erdos_renyi_graph(n, p) for k in range(It)]
-        # M = [nx.to_numpy_matrix(g) if g.number_of_edges() > 0 else np.matrix(np.zeros([n, n])) for g in G]
-
-        # V = [np.linalg.eigh(m) for m in M]
-
-        # print(V[0][1])
 
         count = 0
         av = 0
@@ -36,20 +31,28 @@ for i, n in enumerate(N):
         z2 = av
 
         print(f"Noiseless count: {count}")
-        print("Iteration: " + str(i * len(P) + j + 1) + " of " + str(len(N) * len(P)))
+        print(f"Noiseless average: {av}")
+        print("Iteration: " + str(j * len(P) + i + 1) + " of " + str(len(N) * len(P)))
 
-        if z != 0:
-            row.append(z if z != 0 else 0.5e-0)
-        if av != 0:
-            row2.append(av if av != 0 else 0.5e-0)
+        row.append(z if z != 0 else 0.5e-1)
+        row2.append(z2 if av != 0 else 0.5e-6)
 
     ratios.append(row)
     ratios2.append(row2)
 
-plt, ax = plt.subplots(figsize=(6, 6))
-im = ax.imshow(ratios, cmap='hot', interpolation='nearest', extent=[N[1], N[-1], P[0], P[-1]],
-               aspect='auto', norm=cl.LogNorm())
-plt.colorbar(im)
+# ratios = np.array(ratios, float)
+# ratios2 = np.array(ratios2, float)
+
+fig, ax = plt.subplots(2, 1, figsize=(6, 12))
+im2 = ax[0].imshow(ratios2, cmap='hot', interpolation='nearest', extent=[P[1], P[-1], N[0], N[-1]],
+                   aspect='auto', norm=cl.LogNorm())
+ax[0].set_title("Averages")
+fig.colorbar(im2, ax=ax[0])
+
+im = ax[1].imshow(ratios, cmap='hot', interpolation='nearest', extent=[P[1], P[-1], N[0], N[-1]],
+                  aspect='auto', norm=cl.LogNorm())
+ax[1].set_title("Probability of 1")
+fig.colorbar(im, ax=ax[1])
 plt.show()
 
 nx.readwrite.gexf.write_gexf(G[0], os.path.dirname(__file__) + "\\Graphs\\0.gexf")
