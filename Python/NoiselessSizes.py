c09ffdae44a9913ec188f45795c23f2aafc26d95
Mark Weerheim <theDutchFlamingo@gmail.com>
2020-05-02 15:35:51 +0200

    Added a helper class

--- Python/NoiselessSizes.py
+++ Python/NoiselessSizes.py
@@ -5,9 +5,11 @@ p1 = .05
 N = 15
 nx.laplacian_matrix(random_graph(N, p1)).toarray()
 
-column_labels = list('ABCD')
-row_labels = list('WXYZ')
-data = np.random.rand(N, N)
+data = np.zeros([N, N])
+data[1, -1] = .4
+data[3, -1] = .25
+data[5, -1] = .15
+data[7, -1] = .08
 
 for i in range(N):
     for j in range(0, i):
@@ -17,9 +19,10 @@ fig, ax = plt.subplots()
 heatmap = ax.pcolor(data, cmap=plt.cm.YlGnBu, edgecolor='white', linewidths=5)
 
 # put the major ticks at the middle of each cell
-ax.set_xticks(np.arange(data.shape[0])+0.5, minor=False)
-ax.set_yticks(np.arange(data.shape[1])+0.5, minor=False)
+# ax.set_xticks(np.arange(data.shape[0])+0.5, minor=False)
+# ax.set_yticks(np.arange(data.shape[1])+0.5, minor=False)
 
 ax.set_aspect('equal')
+fig.colorbar(heatmap)
 
 plt.show()
