f714ad99bd746b294696be5d9273170ca18c76ed
Mark Weerheim <theDutchFlamingo@gmail.com>
2020-05-02 18:54:41 +0200

    Added calculations
    It's starting to get there but not quite???

--- Python/NoiselessSizes.py
+++ Python/NoiselessSizes.py
@@ -2,30 +2,29 @@ from common import *
 from plotter import *
 import matplotlib.pyplot as plt
 
-p1 = .05
+p1 = .8
 N = 15
+It = 1000
 nx.laplacian_matrix(random_graph(N, p1)).toarray()
 
 data = np.zeros([N, N])
-data[1, -1] = .4
-data[3, -1] = .25
-data[5, -1] = .15
-data[7, -1] = .08
 
 for i in range(N):
     for j in range(0, i):
         data[i, j] = np.nan
 
+for i in range(It):
+    g = nx.erdos_renyi_graph(N, p1)
+    for v in get_noiseless_subsystems(g):
+        comp = get_component_size(g, np.array(v)[0])
+        ns = get_size(np.array(v)[0])
+        
+        print(f"comp: {comp}, ns: {ns}")
+
+        data[ns - 1, comp - 1] += 1 / It / count_noiseless_subsystems(g)
+
 fig, ax = plt.subplots()
 im, cbar = heatmap(data, ax=ax, cmap="YlGnBu")
-# heatmap = ax.pcolor(data, cmap=plt.cm.YlGnBu, edgecolor='white', linewidths=5)
-
-# put the major ticks at the middle of each cell
-# ax.set_xticks(np.arange(data.shape[0])+0.5, minor=False)
-# ax.set_yticks(np.arange(data.shape[1])+0.5, minor=False)
-
-ax.set_aspect('equal')
 ax.invert_yaxis()
-# fig.colorbar(im)
 
 plt.show()
--- Python/common.py
+++ Python/common.py
@@ -5,6 +5,27 @@ import numpy as np
 tol = 1e-15
 
 
+def get_size(ns: np.array):
+    return len([x for x in ns if x != 0])
+
+
+def get_component_size(g: nx.Graph, ns: np.array):
+    m = nx.laplacian_matrix(g).toarray()
+
+    if all(m.dot(ns) == 0):
+        return get_size(ns)
+    else:
+        prev = 0
+        new = get_size(ns)
+
+        while prev < new:
+            prev = new
+            ns = m.dot(ns)
+            new = get_size(ns)
+
+        return new
+
+
 def random_graph(m, q):
     g = nx.Graph()
 
@@ -14,6 +35,21 @@ def random_graph(m, q):
                 g.add_edge(i, j)
 
     return g
+
+
+def get_noiseless_subsystems(g: nx.Graph):
+    n = g.number_of_nodes()
+    m = nx.to_numpy_matrix(g) if g.number_of_edges() > 0 else np.matrix(np.zeros([n, n]))
+    return get_noiseless_eigenvectors(m)
+
+
+def get_noiseless_eigenvectors(m: np.matrix):
+    vecs = []
+    for v in np.linalg.eigh(m)[1].T:
+        if abs(sum(np.asarray(v)[0])) < tol:
+            vecs.append(v)
+
+    return vecs
 
 
 def has_noiseless_subsystem(g: nx.Graph):
