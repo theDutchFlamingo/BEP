f6c0bcf7a66e60e82800652268bd60d2714f16cd
Mark Weerheim <theDutchFlamingo@gmail.com>
2020-05-02 14:50:41 +0200

    Basic plot looks

--- Python/NoiselessSizes.py
+++ Python/NoiselessSizes.py
@@ -7,9 +7,14 @@ nx.laplacian_matrix(random_graph(N, p1)).toarray()
 
 column_labels = list('ABCD')
 row_labels = list('WXYZ')
-data = np.random.rand(N,N)
+data = np.random.rand(N, N)
+
+for i in range(N):
+    for j in range(0, i):
+        data[i, j] = np.nan
+
 fig, ax = plt.subplots()
-heatmap = ax.pcolor(data, cmap=plt.cm.Blues, edgecolor='white', linewidths=5)
+heatmap = ax.pcolor(data, cmap=plt.cm.YlGnBu, edgecolor='white', linewidths=5)
 
 # put the major ticks at the middle of each cell
 ax.set_xticks(np.arange(data.shape[0])+0.5, minor=False)
@@ -17,10 +22,4 @@ ax.set_yticks(np.arange(data.shape[1])+0.5, minor=False)
 
 ax.set_aspect('equal')
 
-# want a more natural, table-like display
-ax.invert_yaxis()
-ax.xaxis.set_label_position('top') # <-- This doesn't work!
-
-ax.set_xticklabels(row_labels, minor=False)
-ax.set_yticklabels(column_labels, minor=False)
 plt.show()
