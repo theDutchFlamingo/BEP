adfaee6fb30491907381e82c527c5814f5e0eb61
Mark Weerheim <theDutchFlamingo@gmail.com>
2020-05-04 16:55:24 +0200

    Sinful code
    Still doesn't accomplish its goal -_-

--- Python/NoiselessSizes.py
+++ Python/NoiselessSizes.py
@@ -5,26 +5,50 @@ import matplotlib.pyplot as plt
 p1 = .8
 N = 15
 It = 1000
-nx.laplacian_matrix(random_graph(N, p1)).toarray()
 
-data = np.zeros([N, N])
+res = np.zeros([N, N])
 
 for i in range(N):
     for j in range(0, i):
-        data[i, j] = np.nan
+        res[i, j] = np.nan
 
-for i in range(It):
-    g = nx.erdos_renyi_graph(N, p1)
-    for v in get_noiseless_subsystems(g):
-        comp = get_component_size(g, np.array(v)[0])
-        ns = get_size(np.array(v)[0])
-        
-        print(f"comp: {comp}, ns: {ns}")
 
-        data[ns - 1, comp - 1] += 1 / It / count_noiseless_subsystems(g)
+def old_version():
+    for i in range(It):
+        g = nx.erdos_renyi_graph(N, p1)
+        for v in get_noiseless_subsystems(g):
+            comp = get_component_size(g, v)
+            ns = get_size(g, v)
+
+            print(f"comp: {comp}, ns: {ns}")
+
+            res[ns - 1, comp - 1] += 1 / It / count_noiseless_subsystems(g)
+    return res
+
+
+def new_version():
+    for i in range(It):
+        g = nx.erdos_renyi_graph(N, p1)
+        cc = get_connected_components(g)
+        for c in cc:
+            m = nx.to_numpy_array(g)
+            m = m[np.ix_(c, c)]
+            nsc = count_noiseless_eigenvectors(m)
+
+            for v in get_noiseless_eigenvectors(m):
+                lap = np.diag(np.sum(m, axis=0)) - m
+                print(get_connected_count_laplacian(lap, v))
+                comp = get_component_size_laplacian(lap, v)
+                ns = get_size(v)
+
+                res[ns - 1, comp - 1] += 1 / It / len(cc) / nsc
+    return res
+
+
+data = new_version()
 
 fig, ax = plt.subplots()
-im, cbar = heatmap(data, ax=ax, cmap="YlGnBu")
+im, cbar = heatmap(res, ax=ax, cmap="YlGnBu")
 ax.invert_yaxis()
 
 plt.show()
--- Python/common.py
+++ Python/common.py
@@ -1,6 +1,7 @@
 import random
 import networkx as nx
 import numpy as np
+from scipy import linalg, compress, transpose
 
 tol = 1e-15
 
@@ -9,9 +10,7 @@ def get_size(ns: np.array):
     return len([x for x in ns if x != 0])
 
 
-def get_component_size(g: nx.Graph, ns: np.array):
-    m = nx.laplacian_matrix(g).toarray()
-
+def get_component_size_laplacian(m: np.array, ns: np.array):
     if all(m.dot(ns) == 0):
         return get_size(ns)
     else:
@@ -24,6 +23,67 @@ def get_component_size(g: nx.Graph, ns: np.array):
             new = get_size(ns)
 
         return new
+
+
+def get_component_size(g: nx.Graph, ns: np.array):
+    m = nx.laplacian_matrix(g).toarray()
+
+    return get_component_size_laplacian(m, ns)
+
+
+def get_connected_count(g: nx.Graph, ns: np.array):
+    m = nx.laplacian_matrix(g).toarray()
+
+    return get_connected_count_laplacian(m, ns)
+
+
+def get_connected_count_laplacian(m: np.array, ns: np.array):
+    ar = [x for x in range(0, len(ns)) if ns[x] != 0]
+    m = m[np.ix_(ar, ar)]
+
+    return null_space(m).shape[1]
+
+
+def get_connected_components(g: nx.Graph, ns: np.array = None):
+    m = nx.laplacian_matrix(g).toarray()
+
+    if ns is None:
+        ar = range(g.number_of_nodes())
+        ns = np.ones(len(ar))
+    else:
+        ar = [x for x in range(len(ns)) if ns[x] != 0]
+    m = m[np.ix_(ar, ar)]
+
+    visited = [False for x in ns if x != 0]
+
+    def dfs(node):
+        disc = []
+        for v in range(len(visited)):
+            if m[v, node] < 0 and not visited[v]:
+                visited[v] = True
+                disc += [v] + dfs(v)
+        return disc
+
+    ret = []
+    u = 0
+
+    for i in ns:
+        if i == 0:
+            continue
+
+        if not visited[u]:
+            visited[u] = True
+            ret.append([u] + dfs(u))
+
+        u += 1
+
+    return tuple(ret)
+
+
+def null_space(a, eps=1e-15):
+    u, s, vh = linalg.svd(a)
+    null_mask = (s <= eps)
+    return transpose(compress(null_mask, vh, axis=0))
 
 
 def random_graph(m, q):
@@ -39,14 +99,14 @@ def random_graph(m, q):
 
 def get_noiseless_subsystems(g: nx.Graph):
     n = g.number_of_nodes()
-    m = nx.to_numpy_matrix(g) if g.number_of_edges() > 0 else np.matrix(np.zeros([n, n]))
+    m = nx.to_numpy_array(g) if g.number_of_edges() > 0 else np.zeros([n, n])
     return get_noiseless_eigenvectors(m)
 
 
-def get_noiseless_eigenvectors(m: np.matrix):
+def get_noiseless_eigenvectors(m: np.array):
     vecs = []
     for v in np.linalg.eigh(m)[1].T:
-        if abs(sum(np.asarray(v)[0])) < tol:
+        if abs(sum(v)) < tol:
             vecs.append(v)
 
     return vecs
@@ -54,26 +114,26 @@ def get_noiseless_eigenvectors(m: np.matrix):
 
 def has_noiseless_subsystem(g: nx.Graph):
     n = g.number_of_nodes()
-    m = nx.to_numpy_matrix(g) if g.number_of_edges() > 0 else np.matrix(np.zeros([n, n]))
+    m = nx.to_numpy_array(g) if g.number_of_edges() > 0 else np.zeros([n, n])
     return has_noiseless_eigenvector(m)
 
 
-def has_noiseless_eigenvector(m: np.matrix):
+def has_noiseless_eigenvector(m: np.array):
     for v in np.linalg.eigh(m)[1].T:
-        if abs(sum(np.asarray(v)[0])) < tol:
+        if abs(sum(v)) < tol:
             return True
 
 
 def count_noiseless_subsystems(g: nx.Graph):
     n = g.number_of_nodes()
-    m = nx.to_numpy_matrix(g) if g.number_of_edges() > 0 else np.matrix(np.zeros([n, n]))
+    m = nx.to_numpy_array(g) if g.number_of_edges() > 0 else np.zeros([n, n])
     return count_noiseless_eigenvectors(m)
 
 
-def count_noiseless_eigenvectors(m: np.matrix):
+def count_noiseless_eigenvectors(m: np.array):
     count = 0
     for v in np.linalg.eigh(m)[1].T:
-        if abs(sum(np.asarray(v)[0])) < tol:
+        if abs(sum(v)) < tol:
             count += 1
 
     return count
